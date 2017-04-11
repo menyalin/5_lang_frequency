30b259f336de4f11100b087a67bcc2fb754efdc4
Menyalin Alexey <menyalin@icloud.com>
2017-04-11 17:46:05 +0300

    Исправлена функция считывания данных. упростил функции. убрал разделение на 2 файла. описал функции в readme.md

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,19 +1,28 @@
-from pprint import pprint
-from collections import Counter
+import os
 import re
-from function import*
+from collections import Counter
+from pprint import pprint
+
+
+def load_data(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file_handler:
+            return file_handler.read()
+
+
+def get_most_frequent_words(text, num_of_words):
+    text_tmp = text.lower()
+    words = re.findall(r'\w+', text_tmp)
+    counts = Counter(words)
+    return counts.most_common(num_of_words)
 
 
 if __name__ == '__main__':
     NUM_POPULAR_WORDS = 10
-    while True:
-        print('Укажите текстовый файл для анализа, для выхода из программы введите "exit"')
-        path = input()
-        if path == 'exit':
-            exit()
-        text = load_data(path)
-        if text:
-            break
-    get_most_frequent_words(text, NUM_POPULAR_WORDS)
-
-
+    file_path = input("Select data file:")
+    text = load_data(file_path)
+    if not text:
+        print('File not exists!')
+        exit()
+    print('The most popular words in the text and their count:')
+    pprint(get_most_frequent_words(text, NUM_POPULAR_WORDS))
